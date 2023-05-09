31457061d4ce302e77cf26534218bcb37d79a448
Iscreamvann <kyle.vann1996@icloud.com>
2023-05-09 21:40:10 +0100

    created a functioning list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState(["This is my first task."]);
+  const [newTask, setNewTask] = useState('');
+
+   function addNewTask(){
+    const newList = [...tasks, newTask];
+    // alert(newList.toString())
+    setTasks(newList)
+   }
+
+  const listItems = tasks.map(task => <li>{task}</li>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do App</h1>
+      <input value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="write your task here..." type="text"></input>
+      <button onClick={addNewTask}>Add</button>
+      <h2>List:</h2>
+      <ul>{listItems}</ul>
     </div>
   );
 }
