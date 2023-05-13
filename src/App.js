e942a716f5041a86817d694b8919c0497f5427de
David Vann <david@david.com>
2023-05-13 21:59:54 +0200

    completed functionality of to do list app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
+ 
+const cookies = new Cookies();
 
 function App() {
 
-  const [tasks, setTasks] = useState(["This is my first task."]);
+  const [tasks, setTasks] = useState(cookies.get("myTasks")||[]);
   const [newTask, setNewTask] = useState('');
+
+  // useEffect(() => {
+
+  //   const savedTasks = cookies.get("myTasks")
+  //   console.log("saved cookies:", savedTasks);
+  //   setTasks(savedTasks)
+
+  // }, [])
 
    function addNewTask(){
     const newList = [...tasks, newTask];
     // alert(newList.toString())
-    setTasks(newList)
+    setTasks(newList) 
+    setCookies(newList)
    }
+
+   const setCookies=(newTasks)=>{
+    cookies.set("myTasks",newTasks)
+
+   }
+
+   
 
 
   const listItems = tasks.map((task) => { 
     return (<li>
               {task}
-              <button onClick={()=>{ 
-                setTasks(
-                  tasks.filter(
-                    (item)=> task != item
-                    ))
-                  }
+              <button onClick={
+                () => { 
+              
+                const deletedTaskList = tasks.filter(
+                  (item) => task != item
+                  )
+
+                setTasks(deletedTaskList);
+                setCookies(deletedTaskList)
+                }
              }>Delete</button>
             </li>)
   }); // = [<li>This is my first task<button ></li>, <li>second task</li>]
